ce0b5608625130bcd9a4550f2faf346a175903c5
Abdul Samad <samadcom797@gmail.com>
2024-12-04 13:59:00 +0500

    Studying inheritance

---
+++ Inheritance/inheritance_modes.cpp
@@ -0,0 +1,8 @@
+//Modes of inheritance
+
+/*              Derived class     Derived class     Derived class 
+/* Base class   Private mode      Protected Mode    Public Mode
+   Private      Not inherited     Not inherited     Not inherited
+   Protected    Private           Protected         Protected
+   Public       Private           Protected         Public  
+*/
